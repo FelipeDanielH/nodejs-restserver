b6600fb227bfe7f23d6ce172300d2320aa621537
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2024-04-04 17:36:43 -0300

    feat: agrega middleware y ruta
    ahora hay directorio publico para trabajar con una web y la ruta que habia anteriormente es reemplazada por una ruta aparte

--- models/server.js
+++ models/server.js
@@ -8,17 +8,26 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
 
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de la aplicacion
         this.routes();
     }
 
+    middlewares() {
+        // Directorio publico
+        this.app.use( express.static('public') );
+    }
+
     routes() {
-        this.app.get('/', (req, res) => {
+        this.app.get('/api', (req, res) => {
             res.send('hola mundo');
         })
     }
 
     listen() {
-        this.app.listen(this.port, ()=>{
+        this.app.listen(this.port, () => {
             console.log('Servidor corriendo en puerto: ', this.port);
         });
     }
