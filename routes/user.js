46b53b00a5035c085097863055915bdbef28b982
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2024-04-17 10:45:14 -0400

    agrega validaciones

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { usuariosGet, usuariosPut, usuariosPatch, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validarCampos');
 
 const router = Router();
 
@@ -12,6 +13,10 @@ router.patch('/', usuariosPatch);
 
 router.post('/', [
     check('correo', 'El correo no es valido').isEmail(),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña debe tener mas de 6 letras').isLength({min: 6}),
+    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
 ], usuariosPost);
 
 router.delete('/', usuariosDelete);
