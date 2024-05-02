4e624d5e18c15dbd86f28100da3560c32aa892b3
Felipe <felipe.daniel.henriquez@gmail.com>
2024-05-02 17:17:14 -0400

    agrega VENTAS_ROLE al enum de roles

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { usuariosGet, usuariosPut, usuariosPatch, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validarCampos');
+const { esRolValido } = require('../helpers/db-validators');
 
 const router = Router();
 
@@ -15,10 +16,11 @@ router.post('/', [
     check('correo', 'El correo no es valido').isEmail(),
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'La contraseña debe tener mas de 6 letras').isLength({min: 6}),
-    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom( (rol) => esRolValido(rol) ),
     validarCampos
 ], usuariosPost);
 
-router.delete('/', usuariosDelete);
+router.delete('/', usuariosDelete); 
 
 module.exports = router;
