3d2bb63a0039ac34b087932631c6b7a4cdfdb581
Felipe <felipe.daniel.henriquez@gmail.com>
2024-07-24 20:25:58 -0400

    se agrega dedbug

--- database/config.js
+++ database/config.js
@@ -8,7 +8,7 @@ const dbConnection = async() =>{
          console.log('Base de datos ONLINE')
     }catch (error){
         console.log(error);
-        throw new Error('Error a la hora iniciar la base de datos');
+        throw new Error('Error a la hora iniciar la base de datos: ', error);
     }
 }
 
